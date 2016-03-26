a554f6e15988a0b67fac3d86814f16fe473e8056
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-26 01:41:02 +0100

    remove hello world, add hat animation removal

--- src/public/js/index/index.js
+++ src/public/js/index/index.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+const hat = document.getElementById('hat');
+
+hat.addEventListener('click', evt => {
+  hat.classList.add('animated');
+
+  setTimeout(() => hat.classList.remove('animated'), 500);
+});
